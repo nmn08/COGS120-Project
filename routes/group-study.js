4b3a02c49b0d97f248af68e450ed0dd0ce79edfe
Nhat Nguyen <nmn008@ucsd.edu>
2021-02-16 22:57:48 -0800

    Homepage update

---
+++ routes/group-study.js
@@ -0,0 +1,13 @@
+var projects = require('../departments.json');
+
+exports.projectInfo = function(request, response) {
+	var projectID = request.params.id;
+	if (projectID == "random") {
+		projectID = Math.floor(Math.random() * projects.length) + 1;
+	} else {
+		projectID = parseInt(projectID);
+	}
+
+  	var project = projects[projectID-1]; // of by one, our first project has index 0
+  	response.json(project);
+}
