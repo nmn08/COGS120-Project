9da2a8ff4cc0f0351e841e311faf4338afc4ce64
Nhat Nguyen <nmn008@ucsd.edu>
2021-02-17 17:19:06 -0800

    Add group-study page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var grStudy = require('./routes/group-study');
+var dept = require('./routes/departments');
 // Example route
 // var user = require('./routes/user');
 
@@ -38,6 +39,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/group-study', grStudy.view);
+app.get('/dept/:dept', dept.view);
 // Example route
 // app.get('/users', user.list);
 
---
+++ routes/departments.js
@@ -0,0 +1,27 @@
+var groupStudy = require('../group-study.json');
+var departments = require('../departments.json');
+
+exports.view = function(request, response){
+    var dept = request.params.dept;
+    var deptName;
+    console.log(dept);
+    var data = groupStudy["groups"];
+    var retVal = {"groups":[]};
+    // Find the name of the department
+    var depts = departments["departments"];
+    for (var i = 0; i < depts.length; i++) {
+        if (depts[i]["id"] == dept) {
+            deptName = depts[i]["department"];
+            break;
+        }
+    }
+    // Find all course in the department
+    for(var i = 0; i < data.length; i++) {
+        var obj = data[i];
+        if (obj["department"] == dept)
+            retVal["groups"].push(obj)
+    }
+
+    console.log(retVal);
+	response.render('dept', { data: { groups: retVal, dept: deptName} });
+};
